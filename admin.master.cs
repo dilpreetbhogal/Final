225217e0f63e8b8af007e4b94caa259a3bfaf090
GIRBIR <raigurbir070@gmail.com>
2019-08-17 11:44:13 -0400

    admin sheet

---
+++ admin.master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class admin : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Aemail"] == null || Session["Afirstname"] == null)
+        {
+            Response.Redirect("admin_login.aspx");
+        }
+         
+        }
+}
