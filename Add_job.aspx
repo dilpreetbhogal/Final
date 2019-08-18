c877221dd6c8aeaa7fd06156e81f5e0c5cf1acbb
Dilpreet <dilpreetbhogal20@gmail.com>
2019-08-17 22:16:45 -0400

    job

---
+++ Add_job.aspx
@@ -0,0 +1,178 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin.master" AutoEventWireup="true" CodeFile="Add_job.aspx.cs" Inherits="Add_job" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<div class="main">
+<div class="heading">Add Job</div>
+<div class="row">
+<div class="c1"> Main Category</div>
+<div class="c2">
+    <asp:DropDownList ID="ddcategory" runat="server" 
+        DataValueField="maincategoryid" DataTextField="maincategoryname" 
+    AutoPostBack="true" onselectedindexchanged="ddcategory_SelectedIndexChanged"></asp:DropDownList>
+</div>
+<div class="c3"> 
+   
+  </div>
+ </div>
+
+        <div class="row">
+        <div class="c1">Sub category</div>
+       <div class="c2">
+        <asp:DropDownList ID="ddsubcategory" runat="server" DataValueField="subcategoryid" DataTextField="subcategoryname" 
+        AutoPostBack="true" onselectedindexchanged="ddsubcategory_SelectedIndexChanged"></asp:DropDownList>
+        </div>
+         <div class="c3"> 
+         </div>
+          </div>
+
+
+                        <div class="row">
+                      <div class="c1">Child category</div>
+                    <div class="c2">
+                    <asp:DropDownList ID="ddchildcategory" runat="server" DataValueField="childcategoryid" DataTextField="childcategoryname" 
+                    AutoPostBack="true" ></asp:DropDownList>
+                    </div>
+                    <div class="c3">
+                    </div>
+                     </div>
+
+      
+
+ <div class="row">
+<div class="c1">Job Tittle</div>
+<div class="c2">
+    <asp:TextBox ID="txttittle" runat="server">
+    </asp:TextBox>
+</div>
+<div class="c3"> 
+    <asp:RequiredFieldValidator ID="rfvtittle" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txttittle" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator>
+  </div>
+ </div>
+ <div class="row">
+ <div class="c1">Description</div>
+ <div class="c2">
+  <asp:TextBox ID="txtdescription" runat="server" TextMode="MultiLine"> </asp:TextBox>
+ </div>
+ <div class="c3">
+  <asp:RequiredFieldValidator ID="rfvdescription" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txtdescription" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator>
+ </div>
+ </div>
+<div class="row">
+<div class="c1">Qualification</div>
+<div class="c2">
+  <asp:TextBox ID="txtqul" runat="server"></asp:TextBox>
+</div>
+<div class="c3"> 
+<asp:RequiredFieldValidator ID="rfvqul" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txtqul" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator>
+  </div>
+ </div>
+<div class="row">
+<div class="c1">Salary </div>
+<div class="c2">
+    <asp:TextBox ID="txtsalary" runat="server"></asp:TextBox>
+  <asp:Label ID="lblsalary" runat="server" Text="(Per Annum)"></asp:Label>
+</div>
+<div class="c3"> 
+   
+  </div>
+ </div>
+<div class="row">
+<div class="c1"> Designation</div>
+<div class="c2">
+    <asp:TextBox ID="txtdesignation" runat="server" TextMode="MultiLine"></asp:TextBox>
+</div>
+<div class="c3">
+<asp:RequiredFieldValidator ID="rfvdesignation" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txtdesignation" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator> 
+ </div>
+ </div>
+
+<div class="row">
+<div class="c1"> Company Name</div>
+<div class="c2">
+    <asp:DropDownList ID="ddcompanyname" runat="server" DataValueField="Companyid" DataTextField="Companyname" AutoPostBack="true">
+    </asp:DropDownList>
+</div>
+<div class="c3"> 
+    
+  </div>
+ </div>
+
+ <div class="row">
+<div class="c1">Number Of Posts</div>
+<div class="c2">
+    <asp:TextBox ID="txtposts" runat="server">
+    </asp:TextBox>
+</div>
+<div class="c3"> 
+    <asp:RequiredFieldValidator ID="rfvposts" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txtposts" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator>
+  </div>
+ </div>
+
+ <div class="row">
+<div class="c1"> Keys Skills</div>
+<div class="c2">
+    <asp:TextBox ID="txtkeyskills" runat="server" TextMode="MultiLine">
+    </asp:TextBox>
+</div>
+<div class="c3">
+<asp:RequiredFieldValidator ID="rfvkeyskills" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txtkeyskills" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator>
+</div>
+ </div>
+ <div class="row">
+<div class="c1">Country</div>
+<div class="c2">
+    <asp:DropDownList ID="ddcountry" runat="server" DataValueField="Countryid" DataTextField="Countryname" 
+    AutoPostBack="true" onselectedindexchanged="ddcountry_SelectedIndexChanged">
+    </asp:DropDownList>
+</div>
+<div class="c3"> 
+  </div>
+ </div>
+<div class="row">
+<div class="c1"> State</div>
+<div class="c2">
+    <asp:DropDownList ID="ddstate" runat="server" DataValueField="Stateid" DataTextField="Statename" 
+    AutoPostBack="true" onselectedindexchanged="ddstate_SelectedIndexChanged">
+    </asp:DropDownList>
+</div>
+<div class="c3"> 
+    
+  </div>
+ </div>
+  <div class="row">
+<div class="c1"> City</div>
+<div class="c2">
+    <asp:DropDownList ID="ddcity" runat="server" DataValueField="Cityid" DataTextField="Cityname" 
+    AutoPostBack="true">
+    </asp:DropDownList>
+</div>
+<div class="c3">
+  </div>
+ </div>
+ <div class="row">
+ <div class="c1">Last Date To Apply</div>
+ <div class="c2">
+ <asp:TextBox ID="txtdate" runat="server"></asp:TextBox>
+ </div>
+ <div class="c3">
+   <asp:RequiredFieldValidator ID="rfvdate" runat="server" ErrorMessage="Text must not be empty" ControlToValidate="txtdate" ValidationGroup="addjob" Display="Dynamic">
+    </asp:RequiredFieldValidator>
+ </div>
+ 
+ </div>
+ <asp:Button ID="btnaddjob" runat="server" Text="Add Job" ValidationGroup="addjob" 
+        onclick="btnaddjob_Click"/>
+ <asp:Button ID="btnreset" runat="server" Text="Reset" ValidationGroup="addjob" />
+ <div class="error"><asp:Label ID="lblerror" runat="server"></asp:Label>
+</div>
+</div>
+</asp:Content>
+
---
+++ Add_job.aspx.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+public partial class Add_job : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection();
+    SqlCommand cmd = new SqlCommand();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Aemail"] == null || Session["Afirstname"] == null)
+        {
+            Response.Redirect("admin_login.aspx");
+        }
+        else
+        {
+            connection();
+            if (!Page.IsPostBack)
+            {
+
+                selectcountry();
+                selectcompany();
+                selectmaincategory();
+                ddcategory.Items.Insert(0, (new ListItem("select A category", "-1")));
+                ddcountry.Items.Insert(0, (new ListItem("Select A Country", "-1")));
+                ddcompanyname.Items.Insert(0, (new ListItem("Select A Companyname", "-1")));
+            }
+        }
+    }
+
+          void connection()
+    {
+
+        try
+        {
+            con.ConnectionString = " Data source=.\\SQLEXPRESS;AttachDbFilename=" + Server.MapPath("~\\App_Data\\Database.mdf") + ";Integrated Security=True;User Instance=True";
+            
+        }
+        catch (Exception cc)
+        {
+            lblerror.Text = cc.Message;
+        }
+    }
+          void selectcompany()
+          {
+              try
+              {
+                  if (con.State == ConnectionState.Closed)
+                  {
+                      con.Open();
+                  }
+
+                  using (var cmd = new SqlCommand("Select * from [company_name] order by Companyname asc", con))
+                  {
+                      ddcompanyname.DataSource = cmd.ExecuteReader();
+                      ddcompanyname.DataBind();
+                  }
+              }
+              catch (Exception cc)
+              { lblerror.Text = cc.Message; }
+              finally
+              {
+                  if (con.State == ConnectionState.Open)
+                  {
+                      con.Close();
+                  }
+              }
+
+          }
+      void selectcountry()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            using (var cmd = new SqlCommand("Select * from [country] order by Countryname asc", con))
+            {
+                ddcountry.DataSource = cmd.ExecuteReader();
+                ddcountry.DataBind();
+            }
+        }
+        catch (Exception cc)
+        { lblerror.Text = cc.Message; }
+        finally {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+    }
+     void selectstate()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            using (var cmd = new SqlCommand("Select * from [state] where Countryid="+ ddcountry.SelectedValue + "order by (Statename) asc", con))
+            {
+                cmd.Parameters.AddWithValue("@a1", ddcountry.SelectedValue);
+                ddstate.DataSource = cmd.ExecuteReader();
+                ddstate.DataBind();
+                ddstate.Items.Insert(0, (new ListItem("Select A State", "-1")));
+            }
+        }
+        catch (Exception cc)
+        {
+            lblerror.Text = cc.Message;
+        }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+    void selectcity()
+    {
+ try
+ {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            using (var cmd = new SqlCommand("Select * from [city] where Stateid=" + ddstate.SelectedValue + " and Countryid="+ddcountry.SelectedValue+" order by Cityname asc", con))
+            {
+                cmd.Parameters.AddWithValue("@1", ddstate.SelectedValue);
+                ddcity.DataSource = cmd.ExecuteReader();
+                ddcity.DataBind();
+                ddcity.Items.Insert(0, (new ListItem("Select A City", "-1")));
+
+            }
+        }
+        catch (Exception cc)
+        {
+            lblerror.Text = cc.Message;
+        }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+
+    void selectmaincategory()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            using (var cmd = new SqlCommand("Select * from [maincategory1] order by maincategoryname asc", con))
+            {
+                ddcategory.DataSource = cmd.ExecuteReader();
+                ddcategory.DataBind();
+
+            }
+        }
+        catch (Exception cc)
+        { lblerror.Text = cc.Message; }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+    }
+
+ void selectsubcategory()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            using (var cmd = new SqlCommand("Select * from [subcategory] where maincategoryid = '"+ddcategory.SelectedValue+"' order by subcategoryname asc", con))
+            {
+                ddsubcategory.DataSource = cmd.ExecuteReader();
+                ddsubcategory.DataBind();
+                ddsubcategory.Items.Insert(0, (new ListItem("select a subcategory", "-1")));
+            }
+        }
+        catch (Exception cc)
+        { lblerror.Text = cc.Message; }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+    }
+
+    void selectchildcategory()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            using (var cmd = new SqlCommand("Select * from [childcategory] where subcategoryid ='"+ddsubcategory.SelectedValue+"' order by childcategoryname asc", con))
+            {
+                ddchildcategory.DataSource = cmd.ExecuteReader();
+                ddchildcategory.DataBind();
+                ddchildcategory.Items.Insert(0, (new ListItem("select a childcategory", "-1")));
+            }
+        }
+        catch (Exception cc)
+        { lblerror.Text = cc.Message; }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+    }
+    
+ void insert()
+    {
+        try
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            using (var cmd = new SqlCommand("insert into [add_job] (maincategoryid,subcategoryid,childcategoryid,jobtitle,description,qualification,salary,designation,companyid,numberofposts,keyskills,countryid,stateid,cityid,lastdate) values(@c1,@c2,@c3,@c4,@c5,@c6,@c7,@c8,@c9,@c10,@c11,@c12,@c13,@c14,@c15)", con))
+            {
+                cmd.Parameters.AddWithValue("@c1", ddcategory.SelectedValue);
+                cmd.Parameters.AddWithValue("@c2", ddsubcategory.SelectedValue);
+                cmd.Parameters.AddWithValue("@c3", ddchildcategory.SelectedValue);
+                cmd.Parameters.AddWithValue("@c4", txttittle.Text);
+                cmd.Parameters.AddWithValue("@c5", txtdescription.Text);
+                cmd.Parameters.AddWithValue("@c6", txtqul.Text);
+                cmd.Parameters.AddWithValue("@c7", txtsalary.Text);
+                cmd.Parameters.AddWithValue("@c8", txtdesignation.Text);
+                cmd.Parameters.AddWithValue("@c9", ddcompanyname.SelectedValue);
+                cmd.Parameters.AddWithValue("@c10", txtposts.Text);
+                cmd.Parameters.AddWithValue("@c11", txtkeyskills.Text);
+                cmd.Parameters.AddWithValue("@c12", ddcountry.SelectedValue);
+                cmd.Parameters.AddWithValue("@c13", ddstate.SelectedValue);
+                cmd.Parameters.AddWithValue("@c14", ddcity.SelectedValue);
+                cmd.Parameters.AddWithValue("@c15", txtdate.Text);
+           int x = cmd.ExecuteNonQuery();
+                if (x > 0)
+                {
+                    lblerror.Text = "Thank you";
+                }
+                else
+                    lblerror.Text = "error in insert command";
+            
+            }
+        }
+
+        catch (Exception cc)
+        {
+
+
+            lblerror.Text = cc.Message;
+        }
+       
+    }
+
+ protected void btnaddjob_Click(object sender, EventArgs e)
+ {
+     if (ddcountry.SelectedValue == "-1")
+     {
+         lblerror.Text = "please select country";
+     }
+     else
+
+         if (ddstate.SelectedValue == "-1")
+         {
+             lblerror.Text = "please select state";
+         }
+         else
+
+             if (ddcity.SelectedValue == "-1")
+             {
+                 lblerror.Text = "please select city";
+             }
+             else
+                 insert();
+     if (ddcompanyname.SelectedValue == "-1")
+     {
+         lblerror.Text = "please select companyname";
+     }
+
+     if (ddcategory.SelectedValue == "-1")
+     { 
+         lblerror.Text = "Please Select A maincategory"; 
+     }
+     else
+         if (ddsubcategory.SelectedValue == "-1")
+         {
+             lblerror.Text = "Please Select A subcategory"; 
+         }
+     if (ddchildcategory.SelectedValue == "-1")
+     {
+         lblerror.Text = "Please Select A childcategory";
+     }
+     else
+         insert();
+ }
+ 
+protected void ddcategory_SelectedIndexChanged(object sender, EventArgs e)
+{
+    selectsubcategory();
+}
+
+protected void ddsubcategory_SelectedIndexChanged(object sender, EventArgs e)
+{
+    selectchildcategory();
+}
+protected void ddcountry_SelectedIndexChanged(object sender, EventArgs e)
+{
+    selectstate();
+}
+protected void ddstate_SelectedIndexChanged(object sender, EventArgs e)
+{
+    selectcity();
+}
+}
+
+
+
+
+
+
+
+    
